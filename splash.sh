cc3efc6d68ca344668d9564bdc25576ed359fce1
Jason Charney <jrcharney@gmail.com>
2015-01-25 23:06:32 -0600

    Images and Presentation

---
+++ splash.sh
@@ -0,0 +1,193 @@
+#!/bin/bash
+
+clear
+
+cat <<EOF
+
+
+                #####      #####
+                #####      #####                             ###            ###
+                #####      #####                             ###            ###
+                #####      #####                             ###            ###
+                ################                             ###         #########
+                ################     #### ###     ######     ###   ###   #########       #####     ### ####
+                ################    #########    ########    ###  ###       ###         #######    #########
+                #####      #####   ###    ###   ###    ###   ### ###        ###        ###   ###   ####   ###
+                #####      #####   ##     ###   ###          ######         ###        ###   ###   ###    ###
+                #####      #####   ###    ###   ###    ###   ### ###        ###   ##   ###   ###   ####   ###
+                #####      #####    #########    ########    ###  ###        #######    #######    #########
+                #####      #####     #### ###     ######     ###   ###        ####       #####     ### ####
+                                                                                                   ###
+                                                                                                   ###
+                                                  A Game by Jason Charney                          ###
+
+EOF
+
+echo "Press Any Key to continue!"
+read -p x
+
+clear
+
+cat <<EOF
+
+
+
+
+
+
+                                                       X     X
+
+
+                                                        _____
+                                                       |     |
+
+
+
+
+                                            Sorry,  I.O.U.  One Video Game!
+
+
+                                 I had an accident!  My computer disk was screwed up.
+
+
+
+
+EOF
+
+read -p x
+
+clear
+
+cat <<EOF
+
+
+
+
+
+
+
+
+
+                             What I can do is offer you my website and the story of this game.
+
+
+
+
+
+
+
+
+
+EOF
+
+read -p x
+
+clear
+
+cat <<EOF
+
+
+
+
+
+                      It's about a guy who built a hacktop, went to the bathroom.
+                      Returned to find it taken out to the parking lot and blown up by overzealous cops
+                      who mistook it for a bomb.  Why is everything cool looking look explosive?!
+
+
+
+
+                      This is why we can't have nice things!
+
+
+
+
+
+EOF
+
+read -p x
+
+clear
+
+cat <<EOF
+
+
+
+
+
+
+
+         At any rate, you see a junkyard where you can build a new lapdock if you find the parts
+         in the junkyard.  Avoid Killer Robots, Complete missions. Make cool future stuff that
+         looks dangerous but is really cool...like a pitbull puppy!
+
+
+
+         (Say "Puppy!"  It will make you smile.)
+
+
+EOF
+
+read -p x
+
+clear
+
+cat <<EOF
+
+
+
+
+
+
+
+
+
+          It won't be texty like this.  Unless you want it like that...maybe I'll make a mini game.
+
+
+          It's actually going to be a lot like Jetpac (Sinclair ZX Spectrum from the 1980s!)
+          and Rodent's Revenge (Windows Entertainment pack, late 1980s)
+
+
+
+
+
+EOF
+
+read -p x
+
+clear 
+
+cat << EOF
+
+
+
+          Keep up to date!
+
+          Twitter:  @JRCharney
+
+          Hashtag:  #HackTop
+
+          Github:   https://github.com/jrcharney/hacktop-game
+
+          IRC:      irc://irc.freenode.net   #hacktops
+
+          Better games soon!
+
+          Learn how to make a REAL hacktop...then scare the hell out of the TSA ;)
+
+          Raspberry Pi:  https://github.com/jrcharney/kang/How_to_build_a_pi_hacktop.md
+
+          ODroid:        https://github.com/jrcharney/kodos/how_to_build_an_odroid_hacktop.md
+
+          Thanks for having me speak, Global Game Jammers!  See you next time!
+
+
+
+EOF
+
+read -p x
+
+clear
+
+# 0606164
